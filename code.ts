3847015d593c6bace44dc56e7ea712545c1b9c98
Paul MAZEAU <mazeau.paul@gmail.com>
2023-12-04 18:24:25 +0100

    Patch image display

--- code.ts
+++ code.ts
@@ -1,4 +1,4 @@
-figma.showUI(__html__, { width: 400, height: 300 });
+figma.showUI(__html__, { width: 450, height: 300 });
 
 figma.ui.onmessage = async msg => {
   if (msg.type === 'create-couch-cursor' && msg.imageUrl) {
